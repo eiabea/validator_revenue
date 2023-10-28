ea63603eed7ecd9cf66380e7c776a25a0dd09499
eiabea <developer@eiabea.com>
2023-10-28 12:11:35 +0200

    Upgrade packages
    Move cron job to service
    Hard code pattern to run every 15min
    Minor fixes
    Version bump to 1.3.0

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,39 +1,12 @@
 import { Controller, Get, OnModuleInit } from '@nestjs/common';
-import { Logger } from '@nestjs/common';
 import { AppService } from './app.service';
-import { FetchService } from './fetch/fetch.service';
-import { SchedulerRegistry } from '@nestjs/schedule';
-import { InfluxService } from './influx/influx';
-import { ConfigService } from '@nestjs/config';
-import { CronJob } from 'cron';
-import { Perf } from './interfaces/validators';
 
 @Controller()
-export class AppController implements OnModuleInit {
-
-  private readonly logger = new Logger(AppController.name);
-
-  private CRON_PATTERN: string = this.configService.get<string>('CRON_PATTERN', '0 0 * * * *');
+export class AppController {
 
   constructor(
     private readonly appService: AppService,
-    private readonly configService: ConfigService,
-    private readonly fetchService: FetchService,
-    private readonly influxService: InfluxService,
-    private readonly schedulerRegistry: SchedulerRegistry,
   ) {
-  }
-  async onModuleInit() {
-    this.logger.log(`Adding new cronjob with the pattern: ${this.CRON_PATTERN}`);
-    const fetchJob: CronJob = new CronJob(this.CRON_PATTERN, async () => {
-      this.logger.log('Fetch job triggered');
-      const price = await this.fetchService.getLatestPrice();
-      const performances: Array<Perf> = await this.fetchService.getPerformance();
-      this.influxService.write(price, performances);
-      this.logger.log('Successfully finished fetch job');
-    });
-    this.schedulerRegistry.addCronJob('fetch-job', fetchJob);
-    fetchJob.start();
   }
 
   @Get('/health')
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { Perf } from './interfaces/validators';
+import { FetchService } from './fetch/fetch.service';
+import { InfluxService } from './influx/influx';
 
 @Injectable()
 export class AppService {
+
+  private readonly logger = new Logger(AppService.name);
+
+  constructor(
+    private readonly fetchService: FetchService,
+    private readonly influxService: InfluxService,
+  ) {
+  }
+
+  @Cron('0 */15 * * * *')
+  async fetch() {
+    this.logger.log('Fetch job triggered');
+    const price = await this.fetchService.getLatestPrice();
+    const performances: Array<Perf> = await this.fetchService.getPerformance();
+    await this.influxService.write(price, performances);
+    this.logger.log('Successfully finished fetch job');
+  }
+
   getHealth(): string {
     return 'Alive';
   }
